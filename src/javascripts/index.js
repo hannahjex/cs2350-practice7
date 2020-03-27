14cd1529e09e9a6d76e9373a4e8b16cb84181631
Hannah Jex <hannahjex1@mail.weber.edu>
2020-03-27 15:08:17 -0600

    Final submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,58 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+
+import 'bootstrap'
+
+const displayRandomBox = function()
+{
+    let r = Math.random()
+
+    if(r<0.5)
+    {
+        document.write('<div class="green box"></div>')
+    }
+    else
+    {
+        document.write('<div class="black box"></div>')
+    }
+}
+
+function displayNBoxFor(n)
+{
+    for(let i = 0; i < n; i++)
+    {
+        displayRandomBox();
+    }
+}
+
+function displayNBoxWhile(n)
+{
+let j = 0;
+    while(j < nn)
+    {
+        displayRandomBox();
+        j++;
+    }
+}
+
+function displayNBoxDo(n)
+{
+    let k = 0;
+    do
+    {
+        displayRandomBox();
+        k++;
+    }while(k < n);
+}
+
+displayNBoxFor(10);
+displayNBoxFor(10);
+displayNBoxDo(10);
+
+const square = function(x)
+{
+    return x * x;
+}
+
+//alert(square(10))
